a5fd88f7776f1b5dba0ebd1da8e026e8789402c2
sannouki <t.ong25@ncf.edu>
2026-04-09 12:23:40 -0400

    updated 4x2 for videobox

--- frontend/src/components/HomepageComponents/VideoBox.jsx
+++ frontend/src/components/HomepageComponents/VideoBox.jsx
@@ -10,7 +10,7 @@ const VideoBox = ({
 	poster,
 }) => {
 	return (
-		<section className="relative w-full max-w-4xl mx-auto overflow-hidden rounded-2xl p-4 md:p-6">
+		<section className="relative w-full max-w-4x2 mx-auto overflow-hidden rounded-2xl p-4 md:p-6">
 			<div className="relative h-[290px] md:h-[400px]">
 				<video
 					className="w-full h-full object-cover rounded-xl pointer-events-none select-none"
