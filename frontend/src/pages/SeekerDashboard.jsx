1fdd7a2d1c522b69e173132e00b82c027d8baed0
amaringer-ncf <amaringer@ncf.edu>
2026-04-12 17:09:49 -0400

    seekerDashboard updates

--- frontend/src/pages/SeekerDashboard.jsx
+++ frontend/src/pages/SeekerDashboard.jsx
@@ -6,7 +6,7 @@ import { CiBookmark } from "react-icons/ci";
 import Searchbar from "../components/Searchbar.jsx";
 import Pagination from "../components/Pagination.jsx";
 
-const SeekerDashboard = () => {
+const SeekerDashboard = ({ profile, profileImage }) => {
     return (
         <div className="min-h-screen w-full bg-[#FAF3E8]">
             <div className="bg-[#583927] h-screen w-[250px] fixed">
@@ -16,10 +16,30 @@ const SeekerDashboard = () => {
                     <li className="flex items-center gap-2 justify-center"><CiBookmark /><a>Saved</a></li>
                 </ul>
                 <hr className="border-t border-[#FAF3E8] my-4 mr-5 ml-5" />
-                <ul className="fixed left-14 bottom-3 flex flex-col text-2xl">
-                <li className="flex items-center gap-2 justify-center league-gothic-font text-[#FAF3E8]"><a>Profile</a></li>
-                    <li className="flex items-center gap-2 p-2 justify-center"><PinkButton text="Logout" /></li>
-            </ul>
+                <ul className="fixed left-14 bottom-3 flex flex-col text-2xl items-center">
+                    <li className="flex flex-row items-center league-gothic-font text-[#FAF3E8]">
+
+                        <div className="w-16 h-16 rounded-full t-2 scale-65 overflow-hidden border-4 border-[#91D8D4] bg-white flex items-center justify-center">
+                            {profileImage ? (
+                                <img
+                                    src={profileImage}
+                                    alt="Profile"
+                                    className="w-full h-full object-cover"
+                                />
+                            ) : (
+                                <span className="text-xl text-[#BB616D] font-bold">
+          {profile?.name?.charAt(0) || "+"}
+        </span>
+                            )}
+                        </div>
+
+                        <a>{profile?.name || "Profile"}</a>
+                    </li>
+
+                    <li className="flex items-center gap-2 p-2 justify-center">
+                        <PinkButton text="Logout" />
+                    </li>
+                </ul>
             </div>
             <nav className="bg-[#FAF3E8] shadow-md w-2/2 h-[80px]">
                 <div> <Searchbar /> </div>
